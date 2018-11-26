fb64f85137e85fe35a56e1683641a1f67805eab4
Max <IMbrera@users.noreply.github.com>
2018-11-26 22:03:10 +0300

    initialize

---
+++ WebOne/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebOne.Models;
+using System.Linq;
+
+
+namespace WebOne.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good Afternoon";
+            return View ("MyView");
+        }
+       
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponses(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+       
+
+    }
+}
---
+++ WebOne/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace WebOne.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Please, enter your name!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter your email adress")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "enter valid email adress!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Enter your numner phone!")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage ="Enter you attend")]
+        public bool ? WillAttend { get; set; }
+    }
+}
